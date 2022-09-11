c294936e13f779379c8330d59ea14ca3460bd9d8
Augusto Britos <augustobritos95@gmail.com>
2022-09-11 12:54:28 -0300

    Agregando ejercicios 2 al 5 de Java Leccion 3

---
+++ Java/Leccion3/EjerciciosCiclos/src/Ciclos05/Ciclos5B.java
@@ -0,0 +1,33 @@
+package Ciclos05;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Ciclos5B {
+
+    public static void main(String[] args) {
+
+        Integer x, count = 0, number;
+        number = (int)(Math.round(Math.random()*100));
+
+        do{
+
+            x = Integer.parseInt(JOptionPane.showInputDialog("Digit a number: "));
+
+            count++;
+
+            if(x > number) {
+
+                System.out.println("Away");
+
+            } else if (x < number) {
+
+                System.out.print("Closer");
+            }
+
+        } while (x != number);
+
+        System.out.println("You got it right in " + count + " attempts !");
+
+    }
+}

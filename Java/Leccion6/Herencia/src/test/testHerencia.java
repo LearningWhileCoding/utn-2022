5aa9d0af76bb2af9e629d5527f0283ae3590a24a
Augusto Britos <augustobritos95@gmail.com>
2022-10-30 15:58:54 -0300

    Clase 9 de Java con herencia

---
+++ Java/Leccion6/Herencia/src/test/testHerencia.java
@@ -0,0 +1,20 @@
+package test;
+
+
+import domain.Cliente;
+import domain.Empleado;
+import java.util.Date;
+
+public class testHerencia {
+
+    public static void main(String[] args) {
+
+        Empleado empleado1 = new Empleado("Homero", 9000.0);
+        System.out.println("empleado1 = " + empleado1);
+
+        Date fecha1 = new Date();
+
+        Cliente cliente1 = new Cliente(fecha1, true, "Bart", 'M', 17, "Av. Evergeen 742");
+        System.out.println("cliente1 = " + cliente1);
+    }
+}

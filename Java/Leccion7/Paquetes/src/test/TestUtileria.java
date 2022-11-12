932da419c004786a212bc5b021d71806822b2f39
Augusto Britos <augustobritos95@gmail.com>
2022-11-12 18:08:30 -0300

    Arreglando el repositorio.

---
+++ Java/Leccion7/Paquetes/src/test/TestUtileria.java
@@ -0,0 +1,17 @@
+package test;
+
+import ar.com.codesystem.Utileria;
+//import static ar.com.codesystem.Utileria.imprimir; // Solamente aplica para métodos estáticos.
+
+
+public class TestUtileria {
+
+    public static void main(String[] args) {
+
+        Utileria.imprimir("Hello World!");
+
+        //imprimir("Imprimiendo con otra sintaxis!");
+
+        //ar.com.codesystem.Utileria.imprimir("Otra sintaxis!!!!!");
+    }
+}

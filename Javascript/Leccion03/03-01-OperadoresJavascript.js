e59c38fa212171bff5d4d98a626349f1ddfc7d73
Augusto Britos <augustobritos95@gmail.com>
2022-11-13 12:58:07 -0300

    Leccion 09 Js

--- Javascript/Leccion03/03-01-OperadoresJavascript.js
+++ Javascript/Leccion03/03-01-OperadoresJavascript.js
@@ -28,3 +28,41 @@ if(dentroRango >= valMin && dentroRango <= valMax) {
 } else {
     console.log("Esta fuera del rango.");
 }
+
+//Ejercicio condicional
+let vacaciones = true, diaDescanso = false;
+if(vacaciones || diaDescanso){
+    console.log("El padre puede asistir al juego.")
+}
+else{
+    console.log("El padre no puede asistir al juego.")
+}
+
+//Operador ternario
+let resultado2 = 3 > 2 ? "Verdadero" : "Falso";
+console.log(resultado2)
+let numero = 12;
+resultado2 = numero % 2 == 0 ? "Es PAR." : "Es IMPAR.";
+console.log(resultado2)
+
+//Conversión de String a Number
+let miNumero = "21"; 
+console.log(typeof miNumero);
+let edad2 = Number(miNumero); //-->Función
+console.log(typeof edad2);
+//Función isNaN
+if(isNaN(edad2)){ //Is Not a Number(devuelve un resultado booleano)
+    console.log("Esta variable no contiene solo numeros.")
+}
+else{
+    if(edad2 >= 18){
+        console.log("Puede votar.");
+    }
+    else{
+        console.log("No puede votar.");
+    }
+}
+
+//Operador ternario
+let resultado3 = edad2 >= 18 ? "Puede votar." : "No puedevotar.";
+console.log(resultado3);

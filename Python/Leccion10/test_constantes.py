c5092258ec46f034cb3c365906d30aea0f57f46a
Augusto Britos <augustobritos95@gmail.com>
2022-11-27 12:11:28 -0300

    clase 14 Python

---
+++ Python/Leccion10/test_constantes.py
@@ -0,0 +1,8 @@
+from Constantes import *
+from Constantes import Matematicas as Mate
+
+print(MI_CONSTANTE)
+# MI_CONSTANTE = 'Reasignacion de valores'
+# print(MI_CONSTANTE) No se debe hacer!
+
+print(Mate)
